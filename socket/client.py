eb8fb819fb7911dd734d3ebffb49e022719f9b68
JhoantanLop <jhooliveira.lopes@gmail.com>
2024-11-30 12:37:32 -0300

    refactor: update key file paths and improve message handling in client and server

--- socket/client.py
+++ socket/client.py
@@ -5,41 +5,44 @@ from threading import Thread
 
 global tcp_con
 
-# Endereço das chaves publicas e provadas
-client_public_key = "CLIENT_PUBLIC_KEY"
-client_private_key = "CLIENT_PRIVATE_KEY"
+# Endereço das chaves publicas e privadas
+serverPubKey = "/home/jhonatan/github/Fateclando/chaves/serverPub.txt"
+clientPriKey = "/home/jhonatan/github/Fateclando/chaves/clientPri.txt"
 
-arq = open(client_public_key,'rb')
-txt = arq.read()
-arq.close()
-
-#Abre o arquivo da chave publica
-with open(client_public_key, 'rb') as f:
+# Abre o arquivo da chave pública do servidor
+with open(serverPubKey, 'rb') as f:
     chave_publica_data = f.read()
-    public_key = rsa.PublicKey.load_pkcs1(chave_publica_data, format='PEM')
+public_key = rsa.PublicKey.load_pkcs1(chave_publica_data, format='PEM')
 
-#Função para receber as mensagens enviadas pelo servidor
+# Abre o arquivo da chave privada do cliente
+with open(clientPriKey, 'rb') as t:
+    chave_privada_data = t.read()
+private_key = rsa.PrivateKey.load_pkcs1(chave_privada_data, format='PEM')
+
+# Função para receber as mensagens enviadas pelo servidor
 def receber():
     global tcp_con
     while True:
         msg = tcp_con.recv(512)
-        msgd = rsa.decrypt(msg, client_private_key) #Encripta a msg com a chave privada do client
-        print ("Server:",msgd.decode())
+        if not msg:
+            break
+        msgd = rsa.decrypt(msg, private_key)
+        print("Server:", msgd.decode())
 
 # Função de envio
 def enviar():
     global tcp_con
-    print ('Para sair use CTRL+X\n')
+    print('Para sair use CTRL+X\n')
     msg = input()
     while msg != '\x18':
-        mensagem_codificada = msg.encode('utf-8') 
-        msgEncriptada = rsa.encrypt(mensagem_codificada,public_key) # decodifica a msg com a chave publica do Server
+        mensagem_codificada = msg.encode('utf-8')
+        msgEncriptada = rsa.encrypt(mensagem_codificada, public_key)
         tcp_con.send(msgEncriptada)
         msg = input()
     tcp_con.close()
-    
-# Endereco IP do Servidor (maquina do servidor) 
-SERVER = '192.168.137.115'
+
+# Endereco IP do Servidor (maquina do Danko)
+SERVER = '127.0.0.1'
 
 # Porta que o Servidor esta escutando
 PORT = 5002
@@ -48,9 +51,11 @@ tcp_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (SERVER, PORT)
 tcp_con.connect(dest)
 
-
-t_rec = Thread(target=receber, args=())
+t_rec = Thread(target=receber)
 t_rec.start()
 
-t_env = Thread(target=enviar, args=())
+t_env = Thread(target=enviar)
 t_env.start()
+
+t_rec.join()
+t_env.join()
--- socket/server.py
+++ socket/server.py
@@ -6,38 +6,41 @@ global tcp_con
 import rsa
 
 # A chave publica da pessoa para quem vou enviar a mensagem
-client_public_key = 'CLIENT_PUBLIC_KEY'
-arq = open(client_public_key,'rb')
-# Carrega o conteúdo do arquivo
-txt = arq.read()
-arq.close()
-
-# Carrega a chave pública do parceiro no formato PEM
-pub = rsa.PublicKey.load_pkcs1(txt, format='PEM')
+# A chave publica da pessoa para quem vou enviar a mensagem
+client_public_key_address = "/home/jhonatan/github/Fateclando/chaves/clientPub.txt"
+with open(client_public_key_address, 'rb') as file:
+    public_key_file = file.read()
+client_public_key = rsa.PublicKey.load_pkcs1(public_key_file, format='PEM')
 
 # Caminho para a minha chave privada
-server_priv_key = 'SERVER_PRIV_KEY'
-arq1 = open(server_priv_key,'rb')
-txt1 = arq1.read()
-arq1.close()
+server_private_key_address = "/home/jhonatan/github/Fateclando/chaves/serverPri.txt"
+with open(server_private_key_address, 'rb') as file:
+    private_server_file = file.read()
+server_private_key = rsa.PrivateKey.load_pkcs1(private_server_file, format='PEM')
 
-# Carrega minha chave privada no formato PEM
-pri = rsa.PrivateKey.load_pkcs1(txt1, format='PEM') 
         
 def enviar():
     global tcp_con
-    print ('Para sair use CTRL+X\n')
+    print('Para sair use CTRL+X\n')
     msg = input()
     while msg != '\x18':
-        mensagem_codificada = msg.encode('utf-8')
-        msg_encriptada = rsa.encrypt(mensagem_codificada,pub)
-        tcp_con.send(msg_encriptada)
+        codified_message = msg.encode('utf-8')
+        cripted_message = rsa.encrypt(codified_message, client_public_key)
+        tcp_con.send(cripted_message)
         msg = input()
     tcp_con.close()
-        
+
+def receber():
+    global tcp_con
+    while True:
+        msg = tcp_con.recv(1024)
+        if not msg:
+            break
+        msgd = rsa.decrypt(msg, server_private_key)
+        print("Cliente:", msgd.decode())
 
 # Endereco IP do Servidor
-HOST = ''
+HOST = '127.0.0.1'
 
 # Porta que o Servidor vai escutar
 PORT = 5002
@@ -47,20 +50,18 @@ orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
 
-t_env = Thread(target=enviar, args=())
+print('Servidor online')
+tcp_con, cliente = tcp.accept()
+print('Conectado por', cliente)
+
+t_rec = Thread(target=receber)
+t_rec.start()
+
+t_env = Thread(target=enviar)
 t_env.start()
 
-while True:
-    print('Servidor online')
-    tcp_con, cliente = tcp.accept()
-    print ('Concetado por ', cliente)
+t_rec.join()
+t_env.join()
 
-    while True:        
-        msg = tcp_con.recv(512)
-        if not msg: break
-        msgd = rsa.decrypt(msg, pri)
-        msgd = msgd.decode()
-        print("Lud:",msgd)
-        
-    print ('Finalizando conexao com Lud', cliente)
-    tcp_con.close()
+print('Finalizando conexao com', cliente)
+tcp_con.close()
